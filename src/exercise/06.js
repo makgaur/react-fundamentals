276aff1c5bb3177ba7a21081e4b75a16b133f9c4
makgaur <makgaur007@gmail.com>
2022-07-08 22:28:46 +0530

    Unit 1

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,28 +4,37 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
+  
+  const [error, setError] = React.useState(null)
 
-  // 🐨 add the onSubmit handler to the <form> below
-
-  // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
+  const userNameInputRef = React.useRef()
+  function handleSubmit(event) {
+    event.preventDefault()
+    let userNameValue = userNameInputRef.current.value
+    onSubmitUsername(userNameValue)
+  }
+  function handleChange(event) {
+    console.log(event.target)
+    debugger
+    let {value} = event.target
+    const isLowerCase = value === value.toLowerCase()
+    setError(isLowerCase ? null : 'Username must be lower case')
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="txt_username">Username:</label>
+        <input
+          ref={userNameInputRef}
+          id="txt_username"
+          type="text"
+          onChange={handleChange}
+        />
+        <div style={{color: 'red'}}>{error}</div>
       </div>
-      <button type="submit">Submit</button>
+      <button disabled={Boolean(error)} type="submit">
+        Submit
+      </button>
     </form>
   )
 }
